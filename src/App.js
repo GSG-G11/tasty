a76e4f3ef26b3bddf53424f8e5926c1d87307705
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-30 11:52:10 +0300

    Fetch data from API Relates #5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    apiData: [],
+  };
+  componentDidMount() {
+    for (let i = 0; i < 10; i++) {
+      fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then((res) => res.json())
+        .then(({ meals: [{ strMeal, strInstructions, strMealThumb }] }) => {
+          this.setState((prevState) => ({
+            apiData: [
+              ...prevState.apiData,
+              {
+                name: strMeal,
+                instructions: strInstructions,
+                image: strMealThumb,
+              },
+            ],
+          }));
+        });
+    }
+  }
+
   render() {
-    return (
-      <div>
-        hello
-      </div>
-    );
+    const { apiData } = this.state;
+    return <div></div>;
   }
 }
 
