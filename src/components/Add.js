1ac50c940a5a3069731de494c04e3d5df4f23d92
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-30 21:42:51 +0300

    edit Navbar Relates #4

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,36 +1,49 @@
 import React from 'react';
 import '../style.css';
 function Add(props) {
-
-  const { handleAddData, name, image, instructions,handleChange,index ,DisplayForm} = props;
+  const {
+    handleAddData,
+    name,
+    image,
+    instructions,
+    handleChange,
+    index,
+    DisplayForm,
+  } = props;
 
   return (
     <div>
-      
-      <form className="form" onSubmit={(e)=>handleAddData(e,index)}>
-        <h2 className='title'>Add Recipy</h2>
-        <label>Recipy Name  </label>
-        <input className="recipe-name"  name="name" value={name} onChange={handleChange} required />
+      <form className="form" onSubmit={(e) => handleAddData(e, index)}>
+        <h2 className="title">Add Recipy</h2>
+        <label>Recipy Name </label>
+        <input
+          className="recipe-name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          required
+        />
 
         <label> Description </label>
-          <textarea
-          className='textarea'
+        <textarea
+          className="textarea"
           value={instructions}
-            name="instructions"
-            onChange={handleChange} 
-            required
-          />
-          
-    
+          name="instructions"
+          onChange={handleChange}
+          required
+        />
+
         <label>Image url</label>
-        <input  name="image"onChange={handleChange}  value={image} required/>   
-        <div className='footer'>
-        <button className='btn' type='submit'> add </button>
-        <a className='btn' onClick={DisplayForm} >close</a>
-
+        <input name="image" onChange={handleChange} value={image} required />
+        <div className="footer">
+          <button className="btn" type="submit">
+            {' '}
+            add{' '}
+          </button>
+          <p className="btn" onClick={DisplayForm}>
+            close
+          </p>
         </div>
-
-        
       </form>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-function Navbar({DisplayForm}) {
-    return (
-        <div>
-            <p onClick={DisplayForm}>
-                <i className='fa fa-plus'></i>
-            </p>
-            
-        </div>
-    );
+function Navbar({ DisplayForm }) {
+  return (
+    <nav className="nav-wrapper">
+      <div className="logo-container">
+        <img
+          src="https://media.discordapp.net/attachments/937648625293598821/958787308973359135/hamburger.png"
+          alt="logo"
+          className="logo"
+        />
+        <h4>Tasty</h4>
+      </div>
+      <form className="form-wrapper">
+        <input className="input" placeholder="Search for recipies" required />
+        <i className="fa fa-search" />
+      </form>
+      <div className="recipies-controllers">
+        <p onClick={DisplayForm}>
+          <i className="fa fa-plus"></i>
+          Add Recipy
+        </p>
+        <p>My recipies</p>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
