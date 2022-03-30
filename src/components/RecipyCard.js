7f1dfb6b23926d2a6f48763d00ec2f1244013ad5
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-30 19:26:10 +0300

    animate read recipe Relates #19

--- src/components/RecipyCard.js
+++ src/components/RecipyCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import '../style.css';
+import AnimateHeight from 'react-animate-height';
 class RecipyCard extends React.Component {
   state = {
     display: false,
+    height: 0,
   };
+
   toggleDispaly = () => {
+    const { height } = this.state;
     this.setState((prevState) => ({
       display: !prevState.display,
+      height: height === 0 ? 'auto' : 0,
     }));
   };
   render() {
+    const { height } = this.state;
     const { recipy, handleDelete } = this.props;
     const { isOwn } = recipy;
     console.log(isOwn);
@@ -31,19 +37,23 @@ class RecipyCard extends React.Component {
         </div>
         <div className="recipy-card__info">
           <h3 className="recipy-card__name">{recipy.name}</h3>
-          {!this.state.display && (
+          {!height && (
             <button className="button" onClick={this.toggleDispaly}>
               Read details
             </button>
           )}
-          {this.state.display && (
-            <div>
-              <p className="recipy-card__description">{recipy.instructions}</p>
-              <button className="button" onClick={this.toggleDispaly}>
-                Read Less
-              </button>
-            </div>
-          )}
+          {
+            <AnimateHeight id="example-panel" duration={1000} height={height}>
+              <div>
+                <p className="recipy-card__description">
+                  {recipy.instructions}
+                </p>
+                <button className="button" onClick={this.toggleDispaly}>
+                  Read Less
+                </button>
+              </div>
+            </AnimateHeight>
+          }
         </div>
       </div>
     );
